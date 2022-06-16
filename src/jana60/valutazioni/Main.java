11558fc61047b867ae0324370b5fe7c09b8ec2e3
tonia <tonia@cia.como>
2022-06-16 14:47:35 +0200

    classe studente

--- src/jana60/valutazioni/Main.java
+++ src/jana60/valutazioni/Main.java
@@ -3,8 +3,31 @@ package jana60.valutazioni;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Classe Main con metodo main in cui create un array di studenti (i dati degli studenti possono essere chiesti all’utente oppure inseriti nel codice con dei valori di test). L’id di ogni studente deve essere univoco nell’array (possiamo usare un numero progressivo). Il programma deve poi stampare a video per ogni studente se è stato promosso o bocciato e quanti studenti sono stati promossi in tutto.*/ 
 
+		
+		Studente tonia = new Studente(209,8,7.3);
+		Studente luisa = new Studente(1409,5,8.5);
+		Studente davide = new Studente(1903,0,7.1);
+		Studente enzo = new Studente(2409,3,5.6);
+		
+		Studente[] studenti = new Studente[4];
+		studenti[0] = tonia;
+		studenti[1] = luisa;
+		studenti[2] = davide;
+		studenti[3] = enzo;
+		
+		
+		for (int i = 0; i < studenti.length; i++) {
+			System.out.println(studenti[i].pagella());
+		}
+
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- src/jana60/valutazioni/Studente.java
+++ src/jana60/valutazioni/Studente.java
@@ -1,5 +1,39 @@
 package jana60.valutazioni;
 
 public class Studente {
-
+/*Classe Studente con tre attributi: 
+ * id dello studente (intero), 
+ * percentuale assenze (intero), 
+ * media voti (numero decimale). 
+ * La classe deve avere un costruttore con parametri e anche 
+ * un metodo che calcola, in base a percentuale delle assenze e media dei voti, se lo studente è promosso o bocciato*/
+	
+	int idStudente;
+	int percAssenze;
+	double mediaVoti;
+	
+	
+	public Studente(int idStudente, int percAssenze, double mediaVoti) {
+		this.idStudente = idStudente;
+		this.percAssenze = percAssenze;
+		this.mediaVoti = mediaVoti;
+	}
+	
+	boolean pagella() {
+		boolean promosso = false;
+		if(percAssenze < 20 && mediaVoti >=6.5) {
+			promosso=true;
+		}
+		else if(percAssenze < 20 && mediaVoti <= 5.5) {
+			promosso=false;
+		}
+		else if(percAssenze > 20 && mediaVoti >=6.5) {
+			promosso = false;
+		}
+		return promosso;
+	}
+		
+		
+	
+	
 }
